a2b4af1d33b2ebff4d686cfc7142fc69d68af5f0
Yanez-Zamora <jyzamora@cs200u-6a.acad.fullerton.edu>
2023-10-31 15:33:33 -0700

    sandwich.cc

--- part-1/sandwich.cc
+++ part-1/sandwich.cc
@@ -9,14 +9,14 @@
 
 int main(int argc, char* argv[]) {
   std::vector<std::string> arguments{argv, argv + argc};
-if (arguments.size() != 4) {
-  std::cout << "Error: Please provide exactly three arguments.\n";
-  return 1;
-}
+  if (arguments.size() != 4) {
+    std::cout << "Error: Please provide exactly three arguments.\n";
+    return 1;
+  }
   std::string protein = arguments.at(1);
   std::string bread = arguments.at(2);
   std::string condiment = arguments.at(3);
   std::cout << "Your order: A " << protein << " sandwich on " << bread
-   << " with " << condiment << ".\n";
+            << " with " << condiment << ".\n";
   return 0;
 }
